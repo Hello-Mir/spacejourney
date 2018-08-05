996019131335511414ba65583cff6901ab938604
Hello-Mir <yessss42@gmail.com>
2018-08-05 23:32:03 +0300

    1. удалены старые комментарии в RunOnly 2. добавлен класс Randomizer: случайное число и определение погоды

--- src/MainObjects/ForRunOnly.java
+++ src/MainObjects/ForRunOnly.java
@@ -6,47 +6,28 @@ public class ForRunOnly {
 
 
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+		System.out.println(Randomizer.random);
+		System.out.println(Randomizer.weatherGenerate(Randomizer.random));
+		}
 
-
-		Rocket myRocket = new Rocket ("\"Тысячелетний сокол\"");
-		System.out.println ("Имя корабля " + myRocket.name);
-		
-		Rocket.launch("Все в норме");
-//		JOptionPane myPane = new JOptionPane ();
-
-//		Spec medicDefault = new Spec ();
-//		medicDefault.spec = "медик";
-//		medicDefault.strength = 2;
-//		medicDefault.agility = 2;
-//		medicDefault.intelligence = 4;
-//		medicDefault.healScore = 5;
-//		medicDefault.repair = 2;
-//		medicDefault.successLevel = 2;
-//		medicDefault.medicDefaults();
-
-
-
-
-
-
-//		String nameMedic = myPane.showInputDialog("Введите имя медика экипажа");
-//		myPane.showMessageDialog (null,"Имя введено. Есть запись в бортовом журнале!","Успешно",1);
-//		medicDefault.memberName = nameMedic;
+//		JOptionPane myPane = new JOptionPane ();//создание фрейма
 //
+//		Spec medicDefault = new Spec (); //создание объекта типа специальность
+//
+//		String nameMedic = myPane.showInputDialog("Введите имя медика экипажа");//вызов фрейма чтобы ввести имя
+//		myPane.showMessageDialog (null,"Имя введено. Запись передана в бортовой журнал!","Успешно",1);//уведомление об успехе
+//		medicDefault.memberName = nameMedic;
+//		medicDefault.spec = "медик";
+//		medicDefault.medicDefaults();
 //		Crew.showCrewMember(medicDefault,nameMedic);
 
-//		System.out.println(Crew.setName());
-//		Spec mechanickDefault = new Spec();
-//		mechanickDefault.mechanickDefaults();
 
-//		String nameMechanick = myPane.showInputDialog("Введите имя механика экипажа");
-//		myPane.showMessageDialog (null,"Имя введено. Есть запись в бортовом журнале","Успешно!",1);
-//		mechanickDefault.memberName = nameMechanick;
-//		Crew.showCrewMember(mechanickDefault,nameMechanick);
+
+//
+//	}
+//
+//
 	}
-
-
-	}
-	
+//
 
