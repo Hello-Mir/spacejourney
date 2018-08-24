aef7eb328236d92d8b8814c16ebc1a1285c8368c
AABBoy <badboy1134@rambler.ru>
2018-08-25 00:21:23 +0300

    новый класс старт

--- src/MainObjects/start.java
+++ src/MainObjects/start.java
@@ -4,7 +4,7 @@ public class start {
         System.out.println("Ракета готова к старту!  " );
         System.out.println("Отсчет запущен!  " );
         for (int i = 10; i > 0; i--) {
-            System.out.println("     " + i);
+            System.out.println("   " + i);
         }
         System.out.println(" 0! Поехали!  " );
     }
